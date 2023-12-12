dd9cca6640bd56fa1eeac8835a130b88a8d37377
Mouhib777 <mouhibsahbani@gmail.com>
2023-12-12 09:47:43 +0100

    c

--- lib/Model/student.dart
+++ lib/Model/student.dart
@@ -1,14 +1,12 @@
 class Student {
-  final String id;
-  final String firstName;
-  final String lastName;
-  final String email;
-  final String phoneNumber;
-  final String bac;
-  final int iqLevel;
+  String firstName;
+  String lastName;
+  String email;
+  String phoneNumber;
+  String bac;
+  int iqLevel;
 
   Student({
-    this.id = '',
     required this.firstName,
     required this.lastName,
     required this.email,
@@ -16,27 +14,4 @@ class Student {
     required this.bac,
     required this.iqLevel,
   });
-
-  factory Student.fromJson(Map<String, dynamic> json) {
-    return Student(
-      id: json['_id'],
-      firstName: json['firstName'],
-      lastName: json['lastName'],
-      email: json['email'],
-      phoneNumber: json['phoneNumber'],
-      bac: json['bac'],
-      iqLevel: json['iqLevel'],
-    );
-  }
-
-  Map<String, dynamic> toJson() {
-    return {
-      'firstName': firstName,
-      'lastName': lastName,
-      'email': email,
-      'phoneNumber': phoneNumber,
-      'bac': bac,
-      'iqLevel': iqLevel,
-    };
-  }
 }
--- lib/screens/lcs_imStudents.dart
+++ lib/screens/lcs_imStudents.dart
@@ -1,11 +1,8 @@
-import 'dart:convert';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_3/Model/student.dart';
 import 'package:flutter_application_3/screens/addStudent.dart';
 import 'package:flutter_application_3/screens/home.dart';
-import 'package:http/http.dart' as http;
 
 class Lcs_im_Students extends StatefulWidget {
   const Lcs_im_Students({super.key});
@@ -15,47 +12,9 @@ class Lcs_im_Students extends StatefulWidget {
 }
 
 class _Lcs_im_StudentsState extends State<Lcs_im_Students> {
+  List<Student> students = [];
   final String serverUrl = 'http://20.189.116.244:5984';
   final String dbName = 'lcs3im_students';
-  List<Student> students = [];
-  @override
-  void initState() {
-    super.initState();
-    fetchStudents();
-  }
-
-  Future<void> fetchStudents() async {
-    final response = await http.get(Uri.parse('$serverUrl/$dbName/_all_docs'));
-
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body)['rows'];
-      final List<String> studentIds =
-          data.map((row) => row['id'].toString()).toList();
-      final List<Student> fetchedStudents =
-          await Future.wait(studentIds.map((id) => fetchStudent(id)));
-      setState(() {
-        students = fetchedStudents;
-      });
-    } else {
-      print('Failed to fetch students. Status code: ${response.statusCode}');
-    }
-  }
-
-  Future<void> fetchStudent(String id) async {
-    final response = await http.get(Uri.parse('$serverUrl/$dbName/$id'));
-    if (response.statusCode == 200) {
-      final Map<String, dynamic> data = json.decode(response.body);
-      final Student fetchedStudent = Student.fromJson(data);
-      setState(() {
-        students.add(fetchedStudent);
-      });
-    } else {
-      print(
-          'Failed to fetch student with ID $id. Status code: ${response.statusCode}');
-      // Handle the error case if needed
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
