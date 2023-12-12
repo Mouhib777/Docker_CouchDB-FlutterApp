5dac9e8779fd5f9698d891838122a223c6db50dc
Mouhib777 <mouhibsahbani@gmail.com>
2023-12-12 12:40:22 +0100

    c

---
+++ lib/Api_Service/getStudent.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:flutter_application_3/Model/student.dart';
+import 'package:http/http.dart' as http;
+
+class ApiService {
+  final String serverUrl;
+  final String dbName;
+
+  ApiService(this.serverUrl, this.dbName);
+
+  Future<List<Student>> getStudents() async {
+    final response = await http.get(Uri.parse('$serverUrl/$dbName/_all_docs'));
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = json.decode(response.body)['rows'];
+      return data.map((json) => Student.fromJson(json)).toList();
+    } else {
+      throw Exception('Failed to load students');
+    }
+  }
+}
